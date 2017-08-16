2ae43714efc77248fb9e955f0c5b1df102b92638
Krystian Rozbiecki <krystian.r@gmail.com>
2017-08-16 23:15:06 +0200

    1 commit

---
+++ protokolAnulowaniaSprzedazy/src/protokolAnulowaniaSprzedazy/ProtokolAnulowaniaSprzedazy.java
@@ -0,0 +1,43 @@
+package protokolAnulowaniaSprzedazy;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ProtokolAnulowaniaSprzedazy {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProtokolAnulowaniaSprzedazy window = new ProtokolAnulowaniaSprzedazy();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ProtokolAnulowaniaSprzedazy() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
